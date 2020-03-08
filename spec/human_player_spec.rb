35b0a94302664919cb8eaa2d329e49cde5a41580
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-03-08 14:50:33 -0300

    add human player spec

---
+++ spec/human_player_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/human_player.rb'
+require_relative '../lib/board.rb'
+
+RSpec.describe Game::HumanPlayer do
+  let!(:board) { Game::Board.new }
+  subject { described_class.new }
+
+  describe "#spot_to_place_marker" do
+    it { expect(board.available_tile?(subject.spot_to_place_marker(board))).to eq(true)}
+  end
+end
